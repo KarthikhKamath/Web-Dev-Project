7d93f1e46ecb998c2557932a637e3615b1d4b575
Karthik H Kamath <74257697+KarthikhKamath@users.noreply.github.com>
2021-05-16 11:51:34 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+var everything = document.querySelector(".everything")
 window.addEventListener("scroll", function(){
     const header = document.querySelector(".header")
     header.classList.toggle("sticky",window.scrollY >50)
@@ -15,5 +15,14 @@ var links = document.querySelector(".links")
     var navigation = document.querySelector(".navigation")
      navigation.classList.toggle("move")
  })
- var  height= document.querySelector("#wrapper").scrollHeight
- console.log(height)
+ 
+ var mode = document.querySelector(".mode")
+ 
+ mode[0].addEventListener("click", function(){
+    // var mode = document.querySelector(".mode")
+    // var everything = document.querySelector(".everything")
+    everything.classlist.toggle("modeswitch")
+    // console.log("hi")
+    
+    
+ })
